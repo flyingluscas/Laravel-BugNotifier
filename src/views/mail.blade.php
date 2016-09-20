214b6290fb194efb209dba5ca94d9abf2118b8a6
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-09-20 01:15:14 -0300

    Update email view

--- src/views/mail.blade.php
+++ src/views/mail.blade.php
@@ -3,11 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <title>BugNotifier E-mail</title>
+
+    <style type="text/css">
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        pre {
+            max-width: 80%;
+            display: block;
+
+            padding: 16px;
+            margin: 15px auto;
+
+            overflow: auto;
+
+            font-size: 85%;
+            line-height: 1.45;
+
+            border-radius: 3px;
+            background-color: #f7f7f7;
+        }
+    </style>
 </head>
 
 <body>
 
-    {!! nl2br($body) !!}
+    <pre>
+    {{ $body }}
+    </pre>
 
 </body>
 </html>
